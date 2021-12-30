0ec1b81964b1b63fa66d564e37e18651f087973a
ZohanKPI <denizah4022@gmail.com>
2021-12-30 11:22:29 +0200

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,27 +1,67 @@
-def gcd(x, y):
-    if y == 0:
-        return x
-    else:
-        return gcd(y, x % y)
+from math import gcd
 class Rational:
-    def __init__(self,numerator=1,denominator=2):
-        try:
-            if isinstance(numerator,int) and isinstance(denominator,int):
-                self.__numerator = numerator/gcd(numerator,denominator)
-                self.__denominator = denominator/gcd(numerator,denominator)
-                return None
-        except:
-            return None
-    def print_simple(self):
-        try:
-            return str(self.__numerator)+"/"+str(self.__denominator)
-        except:
-            return None
-    def print_float(self):
-        try:
-            return self.__numerator/self.__denominator
-        except:
-            return None
-rational=Rational(2,4)
-print(rational.print_simple())
-print(rational.print_float())
+
+    def __init__(self, numerator, denominator):
+        self.check(numerator, denominator)
+        self.__numerator, self.__denominator = self.simple_form(numerator, denominator)
+
+    def getFraction(self):
+        return f'{self.__numerator} / {self.__denominator}'
+
+    def getFloating(self):
+        return self.__numerator / self.__denominator
+
+    def check(self, numer, denom):
+        if not (isinstance(numer, int) and isinstance(denom, int)):
+            raise TypeError("Wrong value type")
+        if not denom:
+            raise ZeroDivisionError
+
+    def add(self, newnum: int, newdenom: int):
+        self.check(newnum, newdenom)
+        self.__numerator, self.__denominator = self.simple_form(
+            self.__numerator * newdenom + newnum * self.__denominator,
+            self.__denominator * newdenom)
+
+    def sub(self, newnum: int, newdenom: int):
+        self.check(newnum, newdenom)
+        self.__numerator, self.__denominator = self.simple_form(
+            self.__numerator * newdenom - newnum * self.__denominator, self.__denominator * newdenom)
+
+    def mult(self, newnum: int, newdenom: int):
+        self.check(newnum, newdenom)
+        self.__numerator, self.__denominator = self.simple_form(self.__numerator * newnum,
+                                                                 self.__denominator * newdenom)
+
+    def div(self, newnum: int, newdenom: int):
+        self.mult(newdenom, newnum)
+
+    def simple_form(self, numer, denom):
+        k = gcd(numer, denom)
+        return numer // k, denom // k
+
+
+x = Rational(int(input("Enter first argument")), int(input("Enter second argument")))
+
+print(x.getFloating())
+print(x.getFraction())
+
+x.add(4, 5)
+
+print(x.getFloating())
+print(x.getFraction())
+
+x.sub(1, 2)
+
+print(x.getFloating())
+print(x.getFraction())
+
+x.div(4, 5)
+
+print(x.getFloating())
+print(x.getFraction())
+
+x.sub(1, 3)
+
+print(x.getFloating())
+print(x.getFraction())
